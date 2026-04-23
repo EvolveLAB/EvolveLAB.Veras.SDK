2e0b461bc6e625d0c7e26a4799d339abac7389de
Benjamin-Guler <ben@evolvelab.io>
2026-04-23 16:01:14 -0400

    Add devServer configuration with port 8084 in vue.config.js

--- WebAppSample/vue.config.js
+++ WebAppSample/vue.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    port: 8084,
+  },
+
   configureWebpack: {
     devtool: 'source-map',
   },
